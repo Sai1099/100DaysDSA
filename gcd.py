70e5709068f50ae63456837db6261e9db4386a2d
sai1099 <chsaimurali09@gmail.com>
2024-04-18 21:23:48 +0530

    911

---
+++ gcd.py
@@ -0,0 +1,14 @@
+n1 = int(input())
+n2= int(input())
+list1=[]
+for i in range(1,n1):
+    if(n1%i==0):
+     list1.append(i)
+print(list1)
+list4 =[]
+for t in range(1,n2):
+    if(n2%t==0):
+     list4.append(t)
+print(list4)
+y =list(set(list1).intersection(list4))
+print(max(y))
