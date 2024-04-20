aeb70639b27af4e846d9babccd92d5f9becf8846
sai1099 <chsaimurali09@gmail.com>
2024-04-20 12:17:28 +0530

    Day-3

---
+++ check_Str_palindrome.py
@@ -0,0 +1,23 @@
+n= input()
+##slicng a string
+y=n[::-1]
+print(y)
+if(n==y):
+    print("true")
+else:
+    print("flase")
+
+
+
+""""
+def isPalindrome(string: str) -> bool:
+    if (string == string[::-1]): return True
+    return False
+    
+"""
+"""
+def got(n):
+    left=0
+    right = len(n)-1
+    while(left<right):
+"""
--- list_array.py
+++ list_array.py
@@ -1,1 +1,39 @@
 n=int(input())
+
+t=list(input())
+##to know the list size using len
+if(n==len(t)):
+  print(t)
+else:
+  print("enter the arrays correctly")
+
+y = sorted(t)
+
+print(y[::-1])
+##[::-1] reverse the array or list
+
+
+"""
+from typing import *
+
+ 
+
+def reverseArray(n: int, nums: List[int]) -> List[int]:
+
+    def rec(ind, nums):
+
+        if ind == len(nums):
+
+            return []
+
+        res = rec(ind+1, nums)
+
+        res.append(nums[ind])
+
+        return res
+
+ 
+
+    res = rec(0, nums)
+
+    return res"""
